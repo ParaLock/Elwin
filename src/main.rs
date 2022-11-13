a950f3c4d2a9903eac08e3a85ca64ad9939f59db
Nathanael Mercaldo <nmercaldo@irobot.com>
2022-11-12 22:13:11 -0500

    Switched to gtk

--- src/main.rs
+++ src/main.rs
@@ -1,253 +1,139 @@
-mod controls;
-mod scene;
 
-use controls::Controls;
-use scene::Scene;
+use std::cell::RefCell;
+use std::error::Error;
+use std::rc::Rc;
 
-use iced_wgpu::{wgpu, Backend, Renderer, Settings, Viewport};
-use iced_winit::{
-    conversion, futures, program, renderer, winit, Clipboard, Color, Debug,
-    Size,
-};
+use gtk::prelude::*;
+use plotters::prelude::*;
+use plotters_cairo::CairoBackend;
 
-use winit::{
-    dpi::PhysicalPosition,
-    event::{Event, ModifiersState, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-};
+const GLADE_UI_SOURCE: &'static str = include_str!("ui.glade");
 
+#[derive(Clone, Copy)]
+struct PlottingState {
+    mean_x: f64,
+    mean_y: f64,
+    std_x: f64,
+    std_y: f64,
+    pitch: f64,
+    roll: f64,
+}
 
-pub fn main() {
+impl PlottingState {
+    fn guassian_pdf(&self, x: f64, y: f64) -> f64 {
+        let x_diff = (x - self.mean_x) / self.std_x;
+        let y_diff = (y - self.mean_y) / self.std_y;
+        let exponent = -(x_diff * x_diff + y_diff * y_diff) / 2.0;
+        let denom = (2.0 * std::f64::consts::PI / self.std_x / self.std_y).sqrt();
+        let gaussian_pdf = 1.0 / denom;
+        gaussian_pdf * exponent.exp()
+    }
+    fn plot_pdf<'a, DB: DrawingBackend + 'a>(
+        &self,
+        backend: DB,
+    ) -> Result<(), Box<dyn Error + 'a>> {
+        let root = backend.into_drawing_area();
 
-    env_logger::init();
+        root.fill(&WHITE)?;
 
-    // Initialize winit
-    let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
+        let mut chart = ChartBuilder::on(&root).build_cartesian_3d(
+            -10.0f64..10.0,
+            0.0f64..1.2,
+            -10.0f64..10.0,
+        )?;
 
-    let physical_size = window.inner_size();
-    let mut viewport = Viewport::with_physical_size(
-        Size::new(physical_size.width, physical_size.height),
-        window.scale_factor(),
-    );
-    let mut cursor_position = PhysicalPosition::new(-1.0, -1.0);
-    let mut modifiers = ModifiersState::default();
-    let mut clipboard = Clipboard::connect(&window);
+        chart.with_projection(|mut p| {
+            p.pitch = self.pitch;
+            p.yaw = self.roll;
+            p.scale = 0.7;
+            p.into_matrix() // build the projection matrix
+        });
 
-    // Initialize wgpu
-    let default_backend = wgpu::Backends::PRIMARY;
+        chart
+            .configure_axes()
+            .light_grid_style(BLACK.mix(0.15))
+            .max_light_lines(3)
+            .draw()?;
+        let self_cloned = self.clone();
+        chart.draw_series(
+            SurfaceSeries::xoz(
+                (-50..=50).map(|x| x as f64 / 5.0),
+                (-50..=50).map(|x| x as f64 / 5.0),
+                move |x, y| self_cloned.guassian_pdf(x, y),
+            )
+            .style_func(&|&v| (&HSLColor(240.0 / 360.0 - 240.0 / 360.0 * v, 1.0, 0.7)).into()),
+        )?;
 
-    let backend =
-        wgpu::util::backend_bits_from_env().unwrap_or(default_backend);
+        root.present()?;
+        Ok(())
+    }
+}
 
-    let instance = wgpu::Instance::new(backend);
-    let surface = unsafe { instance.create_surface(&window) };
+fn build_ui(app: &gtk::Application) {
+    let builder = gtk::Builder::from_string(GLADE_UI_SOURCE);
+    let window = builder.object::<gtk::Window>("MainWindow").unwrap();
 
-    let (format, (device, queue)) = futures::executor::block_on(async {
-        let adapter = wgpu::util::initialize_adapter_from_env_or_default(
-            &instance,
-            backend,
-            Some(&surface),
-        )
-        .await
-        .expect("No suitable GPU adapters found on the system!");
+    window.set_title("Gaussian PDF Plotter");
 
-        let adapter_features = adapter.features();
-        let needed_limits = wgpu::Limits::default();
+    let drawing_area: gtk::DrawingArea = builder.object("MainDrawingArea").unwrap();
+    let pitch_scale = builder.object::<gtk::Scale>("PitchScale").unwrap();
+    let yaw_scale = builder.object::<gtk::Scale>("YawScale").unwrap();
+    let mean_x_scale = builder.object::<gtk::Scale>("MeanXScale").unwrap();
+    let mean_y_scale = builder.object::<gtk::Scale>("MeanYScale").unwrap();
+    let std_x_scale = builder.object::<gtk::Scale>("SDXScale").unwrap();
+    let std_y_scale = builder.object::<gtk::Scale>("SDYScale").unwrap();
 
-        (
-            surface
-                .get_supported_formats(&adapter)
-                .first()
-                .copied()
-                .expect("Get preferred format"),
-            adapter
-                .request_device(
-                    &wgpu::DeviceDescriptor {
-                        label: None,
-                        features: adapter_features & wgpu::Features::default(),
-                        limits: needed_limits,
-                    },
-                    None,
-                )
-                .await
-                .expect("Request device"),
-        )
+    let app_state = Rc::new(RefCell::new(PlottingState {
+        mean_x: mean_x_scale.value(),
+        mean_y: mean_y_scale.value(),
+        std_x: std_x_scale.value(),
+        std_y: std_y_scale.value(),
+        pitch: pitch_scale.value(),
+        roll: yaw_scale.value(),
+    }));
+
+    window.set_application(Some(app));
+
+    let state_cloned = app_state.clone();
+    drawing_area.connect_draw(move |widget, cr| {
+        let state = state_cloned.borrow().clone();
+        let w = widget.allocated_width();
+        let h = widget.allocated_height();
+        let backend = CairoBackend::new(cr, (w as u32, h as u32)).unwrap();
+        state.plot_pdf(backend).unwrap();
+        Inhibit(false)
     });
 
-    surface.configure(
-        &device,
-        &wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format,
-            width: physical_size.width,
-            height: physical_size.height,
-            present_mode: wgpu::PresentMode::AutoVsync,
-            alpha_mode: wgpu::CompositeAlphaMode::Auto,
-        },
+    let handle_change =
+        |what: &gtk::Scale, how: Box<dyn Fn(&mut PlottingState) -> &mut f64 + 'static>| {
+            let app_state = app_state.clone();
+            let drawing_area = drawing_area.clone();
+            what.connect_value_changed(move |target| {
+                let mut state = app_state.borrow_mut();
+                *how(&mut *state) = target.value();
+                drawing_area.queue_draw();
+            });
+        };
+
+    handle_change(&pitch_scale, Box::new(|s| &mut s.pitch));
+    handle_change(&yaw_scale, Box::new(|s| &mut s.roll));
+    handle_change(&mean_x_scale, Box::new(|s| &mut s.mean_x));
+    handle_change(&mean_y_scale, Box::new(|s| &mut s.mean_y));
+    handle_change(&std_x_scale, Box::new(|s| &mut s.std_x));
+    handle_change(&std_y_scale, Box::new(|s| &mut s.std_y));
+
+    window.show_all();
+}
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("io.github.plotters-rs.plotters-gtk-demo"),
+        Default::default(),
     );
 
-    let mut resized = false;
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
 
-    // Initialize staging belt
-    let mut staging_belt = wgpu::util::StagingBelt::new(5 * 1024);
-
-    // Initialize scene and GUI controls
-    let scene = Scene::new(&device, format);
-    let controls = Controls::new();
-
-    // Initialize iced
-    let mut debug = Debug::new();
-    let mut renderer =
-        Renderer::new(Backend::new(&device, Settings::default(), format));
-
-    let mut state = program::State::new(
-        controls,
-        viewport.logical_size(),
-        &mut renderer,
-        &mut debug,
-    );
-
-    // Run event loop
-    event_loop.run(move |event, _, control_flow| {
-        // You should change this if you want to render continuosly
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent { event, .. } => {
-                match event {
-                    WindowEvent::CursorMoved { position, .. } => {
-                        cursor_position = position;
-                    }
-                    WindowEvent::ModifiersChanged(new_modifiers) => {
-                        modifiers = new_modifiers;
-                    }
-                    WindowEvent::Resized(_) => {
-                        resized = true;
-                    }
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit;
-                    }
-                    _ => {}
-                }
-
-                // Map window event to iced event
-                if let Some(event) = iced_winit::conversion::window_event(
-                    &event,
-                    window.scale_factor(),
-                    modifiers,
-                ) {
-                    state.queue_event(event);
-                }
-            }
-            Event::MainEventsCleared => {
-                // If there are events pending
-                if !state.is_queue_empty() {
-                    // We update iced
-                    let _ = state.update(
-                        viewport.logical_size(),
-                        conversion::cursor_position(
-                            cursor_position,
-                            viewport.scale_factor(),
-                        ),
-                        &mut renderer,
-                        &iced_wgpu::Theme::Dark,
-                        &renderer::Style { text_color: Color::WHITE },
-                        &mut clipboard,
-                        &mut debug,
-                    );
-
-                    // and request a redraw
-                    window.request_redraw();
-                }
-            }
-            Event::RedrawRequested(_) => {
-                if resized {
-                    let size = window.inner_size();
-
-                    viewport = Viewport::with_physical_size(
-                        Size::new(size.width, size.height),
-                        window.scale_factor(),
-                    );
-
-                    surface.configure(
-                        &device,
-                        &wgpu::SurfaceConfiguration {
-                            format,
-                            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-                            width: size.width,
-                            height: size.height,
-                            present_mode: wgpu::PresentMode::AutoVsync,
-                            alpha_mode: wgpu::CompositeAlphaMode::Auto
-                        },
-                    );
-
-                    resized = false;
-                }
-
-                match surface.get_current_texture() {
-                    Ok(frame) => {
-                        let mut encoder = device.create_command_encoder(
-                            &wgpu::CommandEncoderDescriptor { label: None },
-                        );
-
-                        let program = state.program();
-
-                        let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-                        {
-                            // We clear the frame
-                            let mut render_pass = scene.clear(
-                                &view,
-                                &mut encoder,
-                                program.background_color(),
-                            );
-
-                            // Draw the scene
-                            scene.draw(&mut render_pass);
-                        }
-
-                        // And then iced on top
-                        renderer.with_primitives(|backend, primitive| {
-                            backend.present(
-                                &device,
-                                &mut staging_belt,
-                                &mut encoder,
-                                &view,
-                                primitive,
-                                &viewport,
-                                &debug.overlay(),
-                            );
-                        });
-
-                        // Then we submit the work
-                        staging_belt.finish();
-                        queue.submit(Some(encoder.finish()));
-                        frame.present();
-
-                        // Update the mouse cursor
-                         window.set_cursor_icon(
-                             iced_winit::conversion::mouse_interaction(
-                                 state.mouse_interaction(),
-                             ),
-                         );
-
-                        // And recall staging buffers
-                        staging_belt.recall();
-
-                    }
-                    Err(error) => match error {
-                        wgpu::SurfaceError::OutOfMemory => {
-                            panic!("Swapchain error: {}. Rendering cannot continue.", error)
-                        }
-                        _ => {
-                            // Try rendering again next frame.
-                            window.request_redraw();
-                        }
-                    },
-                }
-            }
-            _ => {}
-        }
-    })
+    application.run();
 }
