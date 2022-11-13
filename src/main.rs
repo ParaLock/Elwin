e9187c56a380941ca8cc5735d9b45fbee1996ca3
Nathanael Mercaldo <nmercaldo@irobot.com>
2022-11-12 22:42:58 -0500

    Moved to piston

--- src/main.rs
+++ src/main.rs
@@ -1,139 +1,180 @@
+extern crate piston_window;
+extern crate vecmath;
+extern crate camera_controllers;
+#[macro_use]
+extern crate gfx;
+extern crate shader_version;
 
-use std::cell::RefCell;
-use std::error::Error;
-use std::rc::Rc;
+//----------------------------------------
+// Cube associated data
 
-use gtk::prelude::*;
-use plotters::prelude::*;
-use plotters_cairo::CairoBackend;
+gfx_vertex_struct!( Vertex {
+    a_pos: [i8; 4] = "a_pos",
+    a_tex_coord: [i8; 2] = "a_tex_coord",
+});
 
-const GLADE_UI_SOURCE: &'static str = include_str!("ui.glade");
-
-#[derive(Clone, Copy)]
-struct PlottingState {
-    mean_x: f64,
-    mean_y: f64,
-    std_x: f64,
-    std_y: f64,
-    pitch: f64,
-    roll: f64,
-}
-
-impl PlottingState {
-    fn guassian_pdf(&self, x: f64, y: f64) -> f64 {
-        let x_diff = (x - self.mean_x) / self.std_x;
-        let y_diff = (y - self.mean_y) / self.std_y;
-        let exponent = -(x_diff * x_diff + y_diff * y_diff) / 2.0;
-        let denom = (2.0 * std::f64::consts::PI / self.std_x / self.std_y).sqrt();
-        let gaussian_pdf = 1.0 / denom;
-        gaussian_pdf * exponent.exp()
-    }
-    fn plot_pdf<'a, DB: DrawingBackend + 'a>(
-        &self,
-        backend: DB,
-    ) -> Result<(), Box<dyn Error + 'a>> {
-        let root = backend.into_drawing_area();
-
-        root.fill(&WHITE)?;
-
-        let mut chart = ChartBuilder::on(&root).build_cartesian_3d(
-            -10.0f64..10.0,
-            0.0f64..1.2,
-            -10.0f64..10.0,
-        )?;
-
-        chart.with_projection(|mut p| {
-            p.pitch = self.pitch;
-            p.yaw = self.roll;
-            p.scale = 0.7;
-            p.into_matrix() // build the projection matrix
-        });
-
-        chart
-            .configure_axes()
-            .light_grid_style(BLACK.mix(0.15))
-            .max_light_lines(3)
-            .draw()?;
-        let self_cloned = self.clone();
-        chart.draw_series(
-            SurfaceSeries::xoz(
-                (-50..=50).map(|x| x as f64 / 5.0),
-                (-50..=50).map(|x| x as f64 / 5.0),
-                move |x, y| self_cloned.guassian_pdf(x, y),
-            )
-            .style_func(&|&v| (&HSLColor(240.0 / 360.0 - 240.0 / 360.0 * v, 1.0, 0.7)).into()),
-        )?;
-
-        root.present()?;
-        Ok(())
+impl Vertex {
+    fn new(pos: [i8; 3], tc: [i8; 2]) -> Vertex {
+        Vertex {
+            a_pos: [pos[0], pos[1], pos[2], 1],
+            a_tex_coord: tc,
+        }
     }
 }
 
-fn build_ui(app: &gtk::Application) {
-    let builder = gtk::Builder::from_string(GLADE_UI_SOURCE);
-    let window = builder.object::<gtk::Window>("MainWindow").unwrap();
+gfx_pipeline!( pipe {
+    vbuf: gfx::VertexBuffer<Vertex> = (),
+    u_model_view_proj: gfx::Global<[[f32; 4]; 4]> = "u_model_view_proj",
+    t_color: gfx::TextureSampler<[f32; 4]> = "t_color",
+    out_color: gfx::RenderTarget<::gfx::format::Srgba8> = "o_Color",
+    out_depth: gfx::DepthTarget<::gfx::format::DepthStencil> =
+        gfx::preset::depth::LESS_EQUAL_WRITE,
+});
 
-    window.set_title("Gaussian PDF Plotter");
-
-    let drawing_area: gtk::DrawingArea = builder.object("MainDrawingArea").unwrap();
-    let pitch_scale = builder.object::<gtk::Scale>("PitchScale").unwrap();
-    let yaw_scale = builder.object::<gtk::Scale>("YawScale").unwrap();
-    let mean_x_scale = builder.object::<gtk::Scale>("MeanXScale").unwrap();
-    let mean_y_scale = builder.object::<gtk::Scale>("MeanYScale").unwrap();
-    let std_x_scale = builder.object::<gtk::Scale>("SDXScale").unwrap();
-    let std_y_scale = builder.object::<gtk::Scale>("SDYScale").unwrap();
-
-    let app_state = Rc::new(RefCell::new(PlottingState {
-        mean_x: mean_x_scale.value(),
-        mean_y: mean_y_scale.value(),
-        std_x: std_x_scale.value(),
-        std_y: std_y_scale.value(),
-        pitch: pitch_scale.value(),
-        roll: yaw_scale.value(),
-    }));
-
-    window.set_application(Some(app));
-
-    let state_cloned = app_state.clone();
-    drawing_area.connect_draw(move |widget, cr| {
-        let state = state_cloned.borrow().clone();
-        let w = widget.allocated_width();
-        let h = widget.allocated_height();
-        let backend = CairoBackend::new(cr, (w as u32, h as u32)).unwrap();
-        state.plot_pdf(backend).unwrap();
-        Inhibit(false)
-    });
-
-    let handle_change =
-        |what: &gtk::Scale, how: Box<dyn Fn(&mut PlottingState) -> &mut f64 + 'static>| {
-            let app_state = app_state.clone();
-            let drawing_area = drawing_area.clone();
-            what.connect_value_changed(move |target| {
-                let mut state = app_state.borrow_mut();
-                *how(&mut *state) = target.value();
-                drawing_area.queue_draw();
-            });
-        };
-
-    handle_change(&pitch_scale, Box::new(|s| &mut s.pitch));
-    handle_change(&yaw_scale, Box::new(|s| &mut s.roll));
-    handle_change(&mean_x_scale, Box::new(|s| &mut s.mean_x));
-    handle_change(&mean_y_scale, Box::new(|s| &mut s.mean_y));
-    handle_change(&std_x_scale, Box::new(|s| &mut s.std_x));
-    handle_change(&std_y_scale, Box::new(|s| &mut s.std_y));
-
-    window.show_all();
-}
+//----------------------------------------
 
 fn main() {
-    let application = gtk::Application::new(
-        Some("io.github.plotters-rs.plotters-gtk-demo"),
-        Default::default(),
+    use piston_window::*;
+    use gfx::traits::*;
+    use shader_version::Shaders;
+    use shader_version::glsl::GLSL;
+    use camera_controllers::{
+        FirstPersonSettings,
+        FirstPerson,
+        CameraPerspective,
+        model_view_projection
+    };
+
+    let opengl = OpenGL::V3_2;
+
+    let mut window: PistonWindow =
+        WindowSettings::new("piston: cube", [640, 480])
+        .exit_on_esc(true)
+        .samples(4)
+        .graphics_api(opengl)
+        .build()
+        .unwrap();
+    window.set_capture_cursor(true);
+
+    let mut factory = window.factory.clone();
+
+    let vertex_data = vec![
+        //top (0, 0, 1)
+        Vertex::new([-1, -1,  1], [0, 0]),
+        Vertex::new([ 1, -1,  1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([-1,  1,  1], [0, 1]),
+        //bottom (0, 0, -1)
+        Vertex::new([ 1,  1, -1], [0, 0]),
+        Vertex::new([-1,  1, -1], [1, 0]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        Vertex::new([ 1, -1, -1], [0, 1]),
+        //right (1, 0, 0)
+        Vertex::new([ 1, -1, -1], [0, 0]),
+        Vertex::new([ 1,  1, -1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([ 1, -1,  1], [0, 1]),
+        //left (-1, 0, 0)
+        Vertex::new([-1,  1,  1], [0, 0]),
+        Vertex::new([-1, -1,  1], [1, 0]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        Vertex::new([-1,  1, -1], [0, 1]),
+        //front (0, 1, 0)
+        Vertex::new([-1,  1, -1], [0, 0]),
+        Vertex::new([ 1,  1, -1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([-1,  1,  1], [0, 1]),
+        //back (0, -1, 0)
+        Vertex::new([ 1, -1,  1], [0, 0]),
+        Vertex::new([-1, -1,  1], [1, 0]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        Vertex::new([ 1, -1, -1], [0, 1]),
+    ];
+
+    let index_data: &[u16] = &[
+         0,  1,  2,  2,  3,  0, // top
+         4,  6,  5,  6,  4,  7, // bottom
+         8,  9, 10, 10, 11,  8, // right
+        12, 14, 13, 14, 12, 15, // left
+        16, 18, 17, 18, 16, 19, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
+
+    let (vbuf, slice) = factory.create_vertex_buffer_with_slice
+        (&vertex_data, index_data);
+
+    let texels = [
+        [0xff, 0xff, 0xff, 0x00],
+        [0xff, 0x00, 0x00, 0x00],
+        [0x00, 0xff, 0x00, 0x00],
+        [0x00, 0x00, 0xff, 0x00]
+    ];
+    let (_, texture_view) = factory.create_texture_immutable::<gfx::format::Rgba8>(
+        gfx::texture::Kind::D2(2, 2, gfx::texture::AaMode::Single),
+        gfx::texture::Mipmap::Provided,
+        &[&texels]).unwrap();
+
+    let sinfo = gfx::texture::SamplerInfo::new(
+        gfx::texture::FilterMethod::Bilinear,
+        gfx::texture::WrapMode::Clamp);
+
+    let glsl = opengl.to_glsl();
+    let pso = factory.create_pipeline_simple(
+            Shaders::new()
+                .set(GLSL::V1_20, include_str!("./assets/cube_120.glslv"))
+                .set(GLSL::V1_50, include_str!("./assets/cube_150.glslv"))
+                .get(glsl).unwrap().as_bytes(),
+            Shaders::new()
+                .set(GLSL::V1_20, include_str!("./assets/cube_120.glslf"))
+                .set(GLSL::V1_50, include_str!("./assets/cube_150.glslf"))
+                .get(glsl).unwrap().as_bytes(),
+            pipe::new()
+        ).unwrap();
+
+    let get_projection = |w: &PistonWindow| {
+        let draw_size = w.window.draw_size();
+        CameraPerspective {
+            fov: 90.0, near_clip: 0.1, far_clip: 1000.0,
+            aspect_ratio: (draw_size.width as f32) / (draw_size.height as f32)
+        }.projection()
+    };
+
+    let model = vecmath::mat4_id();
+    let mut projection = get_projection(&window);
+    let mut first_person = FirstPerson::new(
+        [0.5, 0.5, 4.0],
+        FirstPersonSettings::keyboard_wasd()
     );
 
-    application.connect_activate(|app| {
-        build_ui(app);
-    });
+    let mut data = pipe::Data {
+            vbuf,
+            u_model_view_proj: [[0.0; 4]; 4],
+            t_color: (texture_view, factory.create_sampler(sinfo)),
+            out_color: window.output_color.clone(),
+            out_depth: window.output_stencil.clone(),
+        };
 
-    application.run();
+    while let Some(e) = window.next() {
+        first_person.event(&e);
+
+        window.draw_3d(&e, |window| {
+            let args = e.render_args().unwrap();
+
+            window.encoder.clear(&window.output_color, [0.3, 0.3, 0.3, 1.0]);
+            window.encoder.clear_depth(&window.output_stencil, 1.0);
+
+            data.u_model_view_proj = model_view_projection(
+                model,
+                first_person.camera(args.ext_dt).orthogonal(),
+                projection
+            );
+            window.encoder.draw(&slice, &pso, &data);
+        });
+
+        if e.resize_args().is_some() {
+            projection = get_projection(&window);
+            data.out_color = window.output_color.clone();
+            data.out_depth = window.output_stencil.clone();
+        }
+    }
 }
